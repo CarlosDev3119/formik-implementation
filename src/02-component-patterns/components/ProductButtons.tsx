65ce41607bb404b00cdc49e66c485c7adcb7a7b4
Carlosdev3119 <carlosdev3119@gmail.com>
2023-08-10 17:15:11 -0600

    completo

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useContext } from "react";
+import { CSSProperties, useCallback, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 import styles from  '../styles/styles.module.css';
 
@@ -8,8 +8,22 @@ export interface PropsButtons {
 }
 
 export const ProductButtons = ({ className, style }:PropsButtons) => {
-    const {increaseBy, counter } = useContext( ProductContext );
-    
+    const {increaseBy, counter, maxCount } = useContext( ProductContext );
+
+    // const isMatchReach = useCallback(
+    //     () => {
+    //         if(!!maxCount && counter === maxCount){
+    //             return true
+    //         }
+    //         return
+    //     },
+    //     [counter, maxCount]
+    // );
+    const isMatchReach = useCallback(
+        () => !!maxCount && counter === maxCount,
+        [counter, maxCount]
+    )
+
     return (
         <div 
             className={ `${styles.buttonsContainer} ${className}` }
@@ -22,9 +36,9 @@ export const ProductButtons = ({ className, style }:PropsButtons) => {
 
                 <div className={ styles.countLabel }> {counter} </div>
                 <button
-                    className={ styles.buttonAdd }
+                    className={ `${styles.buttonAdd} ${ isMatchReach() && styles.disabled}` }
                     onClick={ () => increaseBy( 1 ) }
-
+                    disabled={ isMatchReach() && true}
                 > + </button>
         </div>
     )
